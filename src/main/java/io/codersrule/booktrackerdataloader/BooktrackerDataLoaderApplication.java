6f44aa6f4d1e7fde134ce47709133461040732f1
codersrule <harounsiddique@gmail.com>
2022-03-05 05:09:59 -0800

    Connected the data loader application to Cassandra successfully. Ready to add data next.

---
+++ src/main/java/io/codersrule/booktrackerdataloader/BooktrackerDataLoaderApplication.java
@@ -0,0 +1,30 @@
+package io.codersrule.booktrackerdataloader;
+
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.DataStaxAstraProperties;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+public class BooktrackerDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktrackerDataLoaderApplication.class, args);
+	}
+
+
+	/* Provides an extra level of security to our database */
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+}
---
+++ src/test/java/io/codersrule/booktrackerdataloader/BooktrackerDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codersrule.booktrackerdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackerDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
